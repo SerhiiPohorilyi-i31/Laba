90c69610088763c81e6ae2c1d532ca85667200ba
SerhiiPohorilyi-i31 <s.pohorilyi@istu.edu.ua>
2024-11-05 22:13:56 +0200

    Add files via upload
    Lab6_task18.3.
    Реалізуйте інтерфейс IEnumerable. Виведіть на консоль список рослин, впорядкований за ціною.

---
+++ Lab6_task18.3.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Рослина : IComparable<Рослина>
+{
+    public string Назва { get; set; }
+    public decimal Ціна { get; set; }
+
+    public Рослина(string назва, decimal ціна)
+    {
+        Назва = назва;
+        Ціна = ціна;
+    }
+
+    // Метод CompareTo для порівняння за ціною
+    public int CompareTo(Рослина інша)
+    {
+        return Ціна.CompareTo(інша.Ціна);
+    }
+
+    public override string ToString()
+    {
+        return $"{Назва} - Ціна: {Ціна} грн";
+    }
+}
+
+public class КолекціяРослин : IEnumerable<Рослина>
+{
+    private List<Рослина> рослини = new List<Рослина>();
+
+    public void ДодатиРослину(Рослина рослина)
+    {
+        рослини.Add(рослина);
+    }
+
+    public IEnumerator<Рослина> GetEnumerator()
+    {
+        return рослини.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    // Метод для впорядкування рослин за ціною
+    public void СортуватиЗаЦіною()
+    {
+        рослини.Sort();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        КолекціяРослин колекція = new КолекціяРослин();
+
+        // Додаємо кілька рослин до колекції
+        колекція.ДодатиРослину(new Рослина("Троянда", 25.50m));
+        колекція.ДодатиРослину(new Рослина("Волошка", 15.75m));
+        колекція.ДодатиРослину(new Рослина("Тюльпан", 35.00m));
+
+        // Сортуємо за ціною
+        колекція.СортуватиЗаЦіною();
+
+        Console.WriteLine("Список рослин, впорядкований за ціною:");
+        foreach (var рослина in колекція)
+        {
+            Console.WriteLine(рослина);
+        }
+    }
+}
+
+
