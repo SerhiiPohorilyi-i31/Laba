5ba90b0cb53893dbc08f1c40950c3c1d8d4c4f67
SerhiiPohorilyi-i31 <s.pohorilyi@istu.edu.ua>
2024-11-17 16:09:12 +0200

    Add files via upload
    Lab7_task18.3. "Універсальні (узагальнені) класи з одним параметром"
    Розробити універсальний клас з 1 параметром. Реалізувати в ньому конструктор і 1 метод для знаходження меншого з двох чисел.

---
+++ Lab7_task18.3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lecture8.Example3
+{
+    // Універсальний клас з одним параметром T
+    class GenericClass<T> where T : IComparable<T>
+    {
+        private T value1;
+        private T value2;
+
+        // Конструктор для ініціалізації значень
+        public GenericClass(T value1, T value2)
+        {
+            this.value1 = value1;
+            this.value2 = value2;
+        }
+
+        // Метод для знаходження меншого з двох чисел
+        public T FindMin()
+        {
+            return value1.CompareTo(value2) < 0 ? value1 : value2;
+        }
+
+        // Метод для відображення значень
+        public void DisplayValues()
+        {
+            Console.WriteLine($"Значення 1: {value1}, Значення 2: {value2}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Приклад з цілими числами
+            GenericClass<int> intPair = new GenericClass<int>(10, 20);
+            intPair.DisplayValues();
+            Console.WriteLine($"Менше значення: {intPair.FindMin()}");
+            Console.WriteLine();
+        }
+    }
+}
+
+
