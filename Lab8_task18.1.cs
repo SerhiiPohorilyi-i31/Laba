34e0ebcf5664b37e295529c3dfa250b339ec5ef5
SerhiiPohorilyi-i31 <s.pohorilyi@istu.edu.ua>
2024-11-24 20:12:33 +0200

    Add files via upload
    Lab8_task18.1 "Просте використання делегатів"
    Написати програму обчислення значень функції у заданій точці x. Значення x вводиться з консолі. Результат вивести на консоль.
    Функція F задається таким чином:
    F(x) = sin2x+1 , x>0
    F(x) = 1-2*sinx,  x<=0
    Кожна функція реалізується окремим методом. Вибір методу делегату виконується при виконанні.

---
+++ Lab8_task18.1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Practical8_Task18_1
+{
+    class Program
+    {
+        // Делегат для обчислення значення функції
+        delegate double Function(double x);
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть значення x: ");
+            if (double.TryParse(Console.ReadLine(), out double x))
+            {
+                // Ініціалізація делегату відповідною функцією
+                Function func = x > 0 ? FunctionForPositiveX : FunctionForNonPositiveX;
+
+                // Виклик делегату та виведення результату
+                double result = func(x);
+                Console.WriteLine($"Результат обчислення F({x}) = {result:F2}");
+            }
+            else
+            {
+                Console.WriteLine("Помилка: введено некоректне значення.");
+            }
+
+            Console.ReadKey();
+        }
+
+        // Функція для x > 0: F(x) = sin²x + 1
+        static double FunctionForPositiveX(double x)
+        {
+            return Math.Pow(Math.Sin(x), 2) + 1;
+        }
+
+        // Функція для x <= 0: F(x) = 1 - 2*sinx
+        static double FunctionForNonPositiveX(double x)
+        {
+            return 1 - 2 * Math.Sin(x);
+        }
+    }
+}
+
+
