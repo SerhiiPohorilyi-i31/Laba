d7b55c5c4f74825ea153079796f4a46f839b6b33
SerhiiPohorilyi-i31 <s.pohorilyi@istu.edu.ua>
2024-12-03 08:23:09 +0200

    Add files via upload
    Lab9_task18.2 "Одиничні лямбда-вирази в делегаті Funс"
    Написати одиничний лямбда-вираз з використанням Func, що отримає 3 параметри (довжини сторін) і визначає, чи можуть вони утворити трикутник.
    Примітка. 
    сума двох будь-яких двох сторін трикутника більша за третю сторону

---
+++ Lab9_task18.2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Лямбда-вираз для перевірки, чи можуть три сторони утворити трикутник
+        Func<double, double, double, bool> isTriangle = (a, b, c) =>
+            a + b > c && a + c > b && b + c > a;
+
+        // Введення довжин сторін
+        Console.Write("Введіть довжину сторони a: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Введіть довжину сторони b: ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.Write("Введіть довжину сторони c: ");
+        double c = double.Parse(Console.ReadLine());
+
+        // Використання лямбда-виразу для перевірки
+        bool result = isTriangle(a, b, c);
+
+        // Виведення результату
+        if (result)
+        {
+            Console.WriteLine("Ці сторони можуть утворити трикутник.");
+        }
+        else
+        {
+            Console.WriteLine("Ці сторони не можуть утворити трикутник.");
+        }
+    }
+}
