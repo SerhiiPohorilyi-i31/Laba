10bda99821c8195b4bb7831604983ac7cd293093
SerhiiPohorilyi-i31 <s.pohorilyi@istu.edu.ua>
2024-12-03 08:32:24 +0200

    Add files via upload
    Lab9_task18.4. "Блокові лямбда-вирази"
    Є назви трьох міст (задати в коді). 
    Написати блоковий лямбда-вираз, який знаходить найдовшу і найкоротшу назви та кількість символів у них. 
    Назви міст передати в блок як параметр. 
    Результат вивести на консоль.

---
+++ Lab9_task18.4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Задати назви міст
+        string[] cities = { "Київ", "Одеса", "Черкаси" };
+
+        // Блоковий лямбда-вираз для визначення найдовшої та найкоротшої назви
+        Action<string[]> findLongestAndShortest = cityNames =>
+        {
+            // Знаходження найдовшої назви
+            string longest = cityNames.OrderByDescending(name => name.Length).First();
+            // Знаходження найкоротшої назви
+            string shortest = cityNames.OrderBy(name => name.Length).First();
+
+            // Виведення результатів
+            Console.WriteLine($"Найдовше місто: {longest} ({longest.Length} символів)");
+            Console.WriteLine($"Найкоротше місто: {shortest} ({shortest.Length} символів)");
+        };
+
+        // Виклик лямбда-виразу
+        findLongestAndShortest(cities);
+    }
+}
+
