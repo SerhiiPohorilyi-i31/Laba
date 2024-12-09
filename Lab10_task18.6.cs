e87978c4cde1b549d0bc4bb1be0e10a9b6d655bc
SerhiiPohorilyi-i31 <s.pohorilyi@istu.edu.ua>
2024-12-09 20:32:35 +0200

    Add files via upload
    Lab10_task18.6. З масиву завд.5 вибрати 3 числа, починаючи з третього.

---
+++ Lab10_task18.6.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Масив чисел
+        int[] numbers = { 42, 32, 25, 18, 27, 58, 78 };
+
+        // Вибір 3 чисел, починаючи з третього (індекс 2)
+        var selectedNumbers = numbers.Skip(2).Take(3);
+
+        // Виведення вибраних чисел
+        Console.WriteLine("Вибрані числа:");
+        foreach (var num in selectedNumbers)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
+
+
