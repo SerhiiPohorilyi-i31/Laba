336e981431dac2ca20886c505b852044e0a199a0
SerhiiPohorilyi-i31 <s.pohorilyi@istu.edu.ua>
2024-12-09 20:16:46 +0200

    Add files via upload
    Lab10_task18.4. Створити у коді колекцію List<T>  з 12 різних доменних імен сайтів. 
    Відібрати  сайти, які закінчуються на  “.ru” та “.ua”. 
    Знайти їх кількість.

---
+++ Lab10_task18.4.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Колекція доменних імен
+        List<string> domains = new List<string>
+        {
+            "google.com",
+            "youtube.com",
+            "rozetka.ua",
+            "ukr.net",
+            "olx.ua",
+            "prom.ua",
+            "tabletki.ua",
+            "auto.ria.com"
+        };
+
+        // Запит LINQ для вибору сайтів, які закінчуються на ".ru" або ".ua"
+        var selectedDomains = domains
+            .Where(domain => domain.EndsWith(".ru") || domain.EndsWith(".ua"))
+            .ToList();
+
+        // Кількість відібраних сайтів
+        int count = selectedDomains.Count;
+
+        // Вивід результатів
+        Console.WriteLine("Сайти, які закінчуються на '.ru' або '.ua':");
+        foreach (var domain in selectedDomains)
+        {
+            Console.WriteLine(domain);
+        }
+
+        Console.WriteLine($"\nКількість таких сайтів: {count}");
+    }
+}
+
