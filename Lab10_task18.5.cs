f6c635c6e64437df81c26c47c8d75f0c6b025ca5
SerhiiPohorilyi-i31 <s.pohorilyi@istu.edu.ua>
2024-12-09 20:22:49 +0200

    Add files via upload
    Lab10_task18.5. Створити у коді масив чисел {42,32,25,18,27, 58,78}.
    Знайти суму чисел, потім різницю чисел.

---
+++ Lab10_task18.5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Масив чисел
+        int[] numbers = { 42, 32, 25, 18, 27, 58, 78 };
+
+        // 1. Знаходимо суму чисел
+        int sum = numbers.Sum();
+
+        // 2. Обчислюємо різницю чисел (42 - 32 - 25 - 18 - 27 - 58 - 78)
+        int difference = numbers.Aggregate((total, next) => total - next);
+
+        // Вивід результатів
+        Console.WriteLine($"Сума чисел: {sum}");
+        Console.WriteLine($"Різниця чисел: {difference}");
+    }
+}
